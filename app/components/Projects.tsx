f419dfebf1637e676b05b04ad959b20db5185bb8
Emiliano <emilianomachado967@gmail.com>
2023-09-20 02:38:23 -0500

    first commit

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -32,7 +32,7 @@ export default async function Projects() {
     <div className="bg-gray-100 min-h-screen py-8">
       <h1 className="text-3xl font-extrabold leading-9 text-gray-900 text-center mb-8">Public Projects</h1>
       <h2 className="text-xl font-light leading-9 text-gray-900 text-center"> You can see the image better by right clicking on it --- Open image in a tab.</h2>
-      <h3 className="text-md font-bold leading-9 text-yellow-500 text-center cursor-pointer"> <a href="https://www.tiktok.com/@itamakingbases?is_from_webapp=1&sender_device=pc">ItamakingBases</a></h3>
+      <h3 className="text-md font-bold leading-9 text-yellow-500 text-center cursor-pointer capitalize" translate="no"> <a href="https://www.tiktok.com/@itamakingbases?is_from_webapp=1&sender_device=pc">ItamakingBases</a></h3>
       <div className="container mx-auto px-4 flex flex-wrap justify-center">
         {data.map((project) => (
           <div key={project._id} className="overflow-hidden bg-white rounded-lg shadow-lg p-4 m-4 max-w-sm">
